83811f7efa93e2074a83a2a4c146ef322ba10605
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-08-26 14:06:37 +0300

    iOSProject initial commit

---
+++ iOSProject/iOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSProject
+//
+//  Created by Aleksandr Voropaev on 8/26/16.
+//  Copyright © 2016 Aleksandr Voropaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
