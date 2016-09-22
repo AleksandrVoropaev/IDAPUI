3e226e5f2d3c0bc1e6268dee1b4e6e555a9d4be3
Aleksandr Voropaev <aleksandr.voropayev@gmail.com>
2016-09-22 19:01:11 +0300

    works with 2 properties: users and tableData

--- iOSProject/Source/UI/AVUsersViewComtroller/AVUsersViewController.m
+++ iOSProject/Source/UI/AVUsersViewComtroller/AVUsersViewController.m
@@ -32,14 +32,14 @@ typedef enum : NSUInteger {
 } AVUsersSortType;
 
 @interface AVUsersViewController ()
-@property (nonatomic, strong)   AVSortingArrayModel     *sortedUsers;
-@property (nonatomic, strong)   AVUsers                 *notSortedUsers;
-@property (nonatomic, assign)   NSUInteger              iterator;
+@property (nonatomic, strong)   AVSortingArrayModel     *tableData;
 @property (nonatomic, assign)   AVUsersSortType         sortType;
 
 @end
 
 @implementation AVUsersViewController
+
+//@dynamic tableData;
 
 #pragma mark -
 #pragma mark Class Methods
@@ -49,41 +49,41 @@ typedef enum : NSUInteger {
 
 - (instancetype)init {
     self = [super init];
-    AVUsers *users = [AVUsers new];
-    self.notSortedUsers = [AVUsers new];
-    [self.notSortedUsers replaceAllObjectsWithObjects:users.objects];
-    self.users = users;
-    self.sortedUsers = [AVSortingArrayModel sortArray:self.users.objects];
+
+    self.sortType = AVUsersSortTypeNotSorted;
+    self.users = [AVUsers new];
+    self.tableData = [AVSortingArrayModel new];
+    [self.tableData replaceAllObjectsWithObjects:self.users.objects];
 
     return self;
 }
 
 - (instancetype)initWithNibName:(nullable NSString *)nibNameOrNil bundle:(nullable NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    AVUsers *users = [AVUsers new];
-    self.notSortedUsers = [AVUsers new];
-    [self.notSortedUsers replaceAllObjectsWithObjects:users.objects];
-    self.users = users;
-    self.sortedUsers = [AVSortingArrayModel sortArray:self.users.objects];
+
+    self.sortType = AVUsersSortTypeNotSorted;
+    self.users = [AVUsers new];
+    self.tableData = [AVSortingArrayModel new];
+    [self.tableData replaceAllObjectsWithObjects:self.users.objects];
 
     return self;
 }
 
 - (nullable instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
-    AVUsers *users = [AVUsers new];
-    self.notSortedUsers = [AVUsers new];
-    [self.notSortedUsers replaceAllObjectsWithObjects:users.objects];
-    self.users = users;
-    self.sortedUsers = [AVSortingArrayModel sortArray:self.users.objects];
-    
+
+    self.sortType = AVUsersSortTypeNotSorted;
+    self.users = [AVUsers new];
+    self.tableData = [AVSortingArrayModel new];
+    [self.tableData replaceAllObjectsWithObjects:self.users.objects];
+
     return self;
 }
 
 #pragma mark -
 #pragma mark Accsessors
 
--(void)setUsers:(AVUsers *)users {
+- (void)setUsers:(AVUsers *)users {
     if (_users != users) {
         _users = users;
         [self.usersView.tableView reloadData];
@@ -109,14 +109,14 @@ typedef enum : NSUInteger {
 #pragma mark UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.users.count;
+    return self.tableData.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     Class cellClass = [AVUserCell class];
 
     AVUserCell *cell = [tableView dequeueReusableCellWithClass:cellClass];
-    cell.user = self.users[indexPath.row];
+    cell.user = self.tableData[indexPath.row];
     
     return cell;
 }
@@ -127,7 +127,7 @@ typedef enum : NSUInteger {
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         [self.users removeObjectAtIndex:indexPath.row];
-//        [self.notSortedUsers removeObjectAtIndex:indexPath.row];
+        [self.tableData removeObjectAtIndex:indexPath.row];
         [self.usersView.tableView deleteRowsAtIndexPaths:@[indexPath]
                                         withRowAnimation:YES];
     }
@@ -139,8 +139,8 @@ typedef enum : NSUInteger {
 {
     [self.users moveObjectFromIndex:[sourceIndexPath indexAtPosition:1]
                             toIndex:[destinationIndexPath indexAtPosition:1]];
-//    [self.notSortedUsers moveObjectFromIndex:[sourceIndexPath indexAtPosition:1]
-//                                     toIndex:[destinationIndexPath indexAtPosition:1]];
+    [self.tableData moveObjectFromIndex:[sourceIndexPath indexAtPosition:1]
+                                toIndex:[destinationIndexPath indexAtPosition:1]];
 }
 
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -158,44 +158,41 @@ typedef enum : NSUInteger {
 - (IBAction)onCreateButton:(id)sender {
     AVUser *user = [AVUser new];
     [self.users addObject:user];
-    [self.sortedUsers addObject:user];
-    [self.notSortedUsers addObject:user];
+    [self.tableData addObject:user];
     [self.usersView.tableView reloadData];
 }
 
 - (IBAction)onSortButton:(id)sender {
-    self.sortType = (self.sortType + 1) % AVUsersSortTypeCount;
-    switch (self.sortType) {
-        AVSwitchCase(AVUsersSortTypeAscending, { [self resortUsers]; });
-        AVSwitchCase(AVUsersSortTypeDescending, { [self resortUsers]; });
+    [self.usersView.tableView setEditing:NO animated:YES];
+    
+    AVUsersSortType sortType = (self.sortType + 1) % AVUsersSortTypeCount;
+    self.sortType = sortType;
+    
+    switch (sortType) {
+        AVSwitchCase(AVUsersSortTypeAscending, {
+            self.usersView.editButton.hidden = YES;
+            self.usersView.createButton.hidden = YES;
+            [self resortUsers];
+        });
+        AVSwitchCase(AVUsersSortTypeDescending, {
+            self.usersView.editButton.hidden = YES;
+            self.usersView.createButton.hidden = YES;
+            [self resortUsers];
+        });
         AVSwitchCase(AVUsersSortTypeNotSorted, {
-            [self.users replaceAllObjectsWithObjects:self.notSortedUsers.objects];
+            [self.tableData replaceAllObjectsWithObjects:self.users.objects];
+            self.usersView.editButton.hidden = NO;
+            self.usersView.createButton.hidden = NO;
             [self.usersView.tableView reloadData];
         });
         AVSwitchCaseDefault({});
     }
-//    if ([self needToSort]) {
-//        [self.sortedUsers resortArray];
-//        [self.users replaceAllObjectsWithObjects:self.sortedUsers.objects];
-//        [self.usersView.tableView reloadData];
-//    } else {
-////        self.users = self.notSortedUsers;
-//        [self.users replaceAllObjectsWithObjects:self.notSortedUsers.objects];
-//        [self.usersView.tableView reloadData];
-//    }
 }
 
 - (void)resortUsers {
-    [self.sortedUsers resortArray];
-    [self.users replaceAllObjectsWithObjects:self.sortedUsers.objects];
+    [self.tableData resortArray];
     [self.usersView.tableView reloadData];
 }
-
-
-//- (BOOL)needToSort {
-//    _iterator = (_iterator + 1) % 3;
-//    return _iterator != 0;
-//}
 
 #pragma mark -
 #pragma mark Private
